628db14d679952880dbe1cb297001ecdc38f9541
Alinka.Sergei <alinka.sergei@gmail.com>
2022-07-13 22:15:25 +0300

    6

---
+++ DZ/D6/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Write("M числа через запятую: ");
+int[] numbers = StringN(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine($"сумма чисел больше 0 = {sum}");
+
+int[] StringN(string Diggits)
+{
+    int count = 1;
+    for (int i = 0; i < Diggits.Length; i++)
+    {
+        if (Diggits[i] == ',')
+        {
+            count++;
+        }
+    }
+    int[] numbers = new int[count];
+    int index = 0;
+    for (int i = 0; i < Diggits.Length; i++)
+    {
+        string temp = "";
+
+        while (Diggits[i] != ',')
+        {
+            if (i != Diggits.Length - 1)
+            {
+                temp += Diggits[i].ToString();
+                i++;
+            }
+            else
+            {
+                temp += Diggits[i].ToString();
+                break;
+            }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5) */
+
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1)/(k1 - k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
